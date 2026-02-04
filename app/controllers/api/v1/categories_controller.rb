914ebe9ac9d425e0348724558b076b5d1151bd15
Tophers-Code <t-man@topher.codes>
2026-02-04 07:42:26 -0700

    Add Categories CRUD

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -2,13 +2,51 @@ module Api
   module V1
     class CategoriesController < BaseController
       def index
-        @categories = Category.all
-        render json: @categories
+        @categories = Category.left_joins(:verses)
+                              .select('categories.*, COUNT(verses.id) as verses_count')
+                              .group('categories.id')
+        render json: @categories.map { |c| category_with_count(c) }
       end
 
       def show
         @category = Category.find(params[:id])
         render json: @category, include: { verses: { include: :bible_book } }
+      end
+
+      def create
+        @category = Category.new(category_params)
+        @category.save!
+        render json: @category, status: :created
+      end
+
+      def update
+        @category = Category.find(params[:id])
+        @category.update!(category_params)
+        render json: @category
+      end
+
+      def destroy
+        @category = Category.find(params[:id])
+        @category.destroy!
+        head :no_content
+      end
+
+      private
+
+      def category_params
+        params.require(:category).permit(:name, :meaning, :color_code)
+      end
+
+      def category_with_count(category)
+        {
+          id: category.id,
+          name: category.name,
+          meaning: category.meaning,
+          color_code: category.color_code,
+          verses_count: category.verses_count.to_i,
+          created_at: category.created_at,
+          updated_at: category.updated_at
+        }
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :bible_books, only: [:index, :show]
-      resources :categories, only: [:index, :show]
+      resources :categories
       resources :verses do
         resources :references, only: [:create, :destroy], controller: 'verse_references'
         member do
